08faca59b4b9d65848dd8474cb296670dd584c6d
jordiedill <jordan.dillard13@gmail.com>
2019-06-10 15:52:36 -0500

    check this project off the todo list

--- app.js
+++ app.js
@@ -1,1 +1,44 @@
-// TODO
+
+
+class GroceryListItems extends React.Component {
+  
+  constructor (props){
+      super(props);
+
+      this.state = {
+          done: false
+      };
+  }
+
+  onListItemClick() {
+      this.setState({
+          done: !this.state.done
+      });
+  }
+
+  render() {
+
+    var style = {
+        textDecoration: this.state.done ? 'line-through' : 'none'
+
+
+    }
+    return (
+          <li style={style} onClick={this.onListItemClick.bind(this)}>{this.props.item}</li>
+      
+        );
+  }
+ 
+}
+
+var GroceryList = (props) => (
+     <ul>
+    {props.items.map(item =>
+      <GroceryListItems item={item}/>
+    )}
+    </ul>
+);
+
+
+
+ReactDOM.render(<GroceryList items={['Bread','milk','smokes']}/>, document.getElementById("app"));
